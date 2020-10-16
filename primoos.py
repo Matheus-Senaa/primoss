0d189f1846659a9171605b5270917a40a10113e1
Matheus-Senaa <itzsena16@gmail.com>
2020-10-15 21:50:07 -0300

    ac3

--- primoos.py
+++ primoos.py
@@ -7,26 +7,25 @@ app = Flask(_name_)
 @app.route('/')
 
 def nao_entre_em_panico():
+limite = 100
+c = 1
+p = 1
+numero = 3
 
-    limite = 100
-    c = 1
-    p = 1
-    numero = 3
-
-    primos = "2, "
-    while p < limite:
-        ehprimo = 1
-        for i in range (2, numero):
-            if numero % i == 0:
-                ehprimo = 0
-                break
-        if (ehprimo):
-            primos = primos + str(numero) + ","
-            p += 1
-            if (p % 10 == 0):
-                    primos = primos +  "<br>"
-        numero +=1
-    return primos    
+primos = "2, "
+ while p < limite:
+    ehprimo = 1
+    for i in range (2, numero):
+        if numero % i == 0:
+            ehprimo = 0
+            break
+    if (ehprimo):
+        primos = primos + str(numero) + ","
+        p += 1
+        if (p % 10 == 0):
+                primos = primos +  "<br>"
+    numero +=1
+return primos    
 
 
 if _name_ == "_main_":
